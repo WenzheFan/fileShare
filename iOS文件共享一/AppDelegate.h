c12417f76ffd4460beb2f499a3d2e365f10a5848
fanwenzhe <523935688@qq.com>
2018-06-04 11:00:01 +0800

    Initial Commit

---
+++ iOS文件共享一/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS文件共享一
+//
+//  Created by apple on 2018/6/4.
+//  Copyright © 2018年 范文哲. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
